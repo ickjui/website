fb7f2585f76a3b87960067640e11bce79cafbabe
ickjui <40743383+ickjui@users.noreply.github.com>
2020-07-30 18:30:50 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,9 @@
-// Write your JS in here
 let pics = [
 	'IMG_2109.JPG',
 	'IMG_0733.JPG',
 	'IMG_2469.jpg',
 	'IMG_1974.jpg',
 	'IMG_1780.jpg'
-	
 ];
 
 const btn = document.querySelector('button');
